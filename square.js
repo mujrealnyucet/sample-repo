5b9967e0af7c8312387abbbf85c2fbd274b256ba
mujrealnyucet <vovox.login@gmail.com>
2026-01-22 17:33:03 +0100

    Remove square root script

--- square.js
+++
@@ -1,1 +0,0 @@
-export const quakeInvSqrt = n => ((b) => ((f, i) => (f[0] = n, i[0] = 0x5f3759df - (i[0] >> 1), f[0] * (1.5 - (0.5 * n * f[0] * f[0]))))(new Float32Array(b), new Int32Array(b)))(new ArrayBuffer(4));
